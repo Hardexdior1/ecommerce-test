dfc756587955778d4ec720f33637065bd98e0903
adeniran <adeniranquwam001@gmail.com>
2022-12-15 08:24:33 +0100

    init commit

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -18,8 +18,8 @@ const LandingPage = ({rate}) => {
       
       <Section1 />
       <Section2  rates={rate}/>
-      <Section3 />
-      <Section4 />
+      {/* <Section3 /> */}
+      {/* <Section4 /> */}
       <Section5 />
       <Section6 />
       <Section7 />
